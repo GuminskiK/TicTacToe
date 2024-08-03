34c58fedaa5fa10b5617f2eac9d8c852f976d887
Gumiński Krzysztof <krzys.gumi@gmail.com>
2024-08-03 21:24:25 +0200

    Ukończono podstawową wersję gry. Dodano sprawdzanie czy i kto zwycięzył, dodano komunikat o zwycięstwie i dodano blokowanie klikania przycisków po nim.

--- SRC/Game.java
+++ SRC/Game.java
@@ -3,13 +3,14 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Game extends JPanel {
 
     private ArrayList<Tile> tiles = new ArrayList<>();
     public char[] gameState = new char[9];
-    private int whichPlayer = 0;
+    private char whichPlayer = 'x';
 
     Game(){
 
@@ -26,17 +27,90 @@ public class Game extends JPanel {
 
     public void changeTileState(int x){
 
-        if (this.whichPlayer % 2 == 0){
+        if (this.whichPlayer == 'x'){
             tiles.set(x, new XTile());
             this.remove(x);
             this.add(tiles.get(x), x);
-        } else if ( this.whichPlayer % 2 == 1){
+            gameState[x] = 'x';
+        } else if ( this.whichPlayer == 'o'){
             tiles.set(x, new OTile());
             this.remove(x);
             this.add(tiles.get(x), x);
+            gameState[x] = 'o';
         }
 
-        whichPlayer++;
+
+
+        if (checkIfVictory(whichPlayer) == true){
+            System.out.println("Zwycięstwo");
+            for(int i = 0; i < 9; i++){
+                tiles.get(i).setEnabled(false);
+            }
+
+            if( this.whichPlayer == 'o'){
+                JOptionPane.showMessageDialog(this, "Zwyciężył gracz władający kółkiem!");
+            } else{
+                JOptionPane.showMessageDialog(this, "Zwyciężył gracz władający krzyżykiem!");
+            }
+            
+        }
+        
+        if (whichPlayer == 'x'){
+            whichPlayer = 'o';
+        } else {
+            whichPlayer = 'x';
+        }
+
         paintAll(getGraphics());
+    }
+
+    private boolean checkIfVictory(char x){
+        
+        if(gameState[0] == x){
+            if( gameState[1] == x && gameState[2] == x){
+              
+                return true;
+           
+            } else if (gameState[3] == x && gameState[6] == x){
+
+                return true;
+            }
+        }
+        
+        if (gameState[4] == x){
+
+            if( gameState[0] == x && gameState[8] == x){
+
+                return true;
+
+            } else if (gameState[2] == x && gameState[6] == x){
+
+                return true;
+
+            } else if (gameState[1] == x && gameState[7] == x){
+
+                return true;
+
+            } else if (gameState[3] == x && gameState[5] == x){
+
+                return true;
+            }
+
+        }
+        
+        if (gameState[8] == x){
+
+            if( gameState[6] == x && gameState[7] == x){
+
+                return true;
+
+            } else if (gameState[2] == x && gameState[5] == x){
+
+                return true;
+
+            }
+        }
+
+        return false;
     }
 }
